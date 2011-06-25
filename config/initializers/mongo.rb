75e401da5c82171d84c9a292fbeb4756c72b2963
Dan Spinosa <spinosa@gmail.com>
2011-06-25 11:00:12 -0400

    Initial commit, w/ mongo

---
+++ config/initializers/mongo.rb
@@ -0,0 +1,23 @@
+MongoMapper.connection = Mongo::Connection.new(
+  APP_CONFIG[:mongo_server], 
+  APP_CONFIG[:mongo_port], 
+  :pool_size => APP_CONFIG[:mongo_pool_size])
+MongoMapper.database = "ct-#{Rails.env}"
+
+if APP_CONFIG[:mongo_username] && APP_CONFIG[:mongo_password]
+  MongoMapper.database.authenticate(APP_CONFIG[:mongo_username], APP_CONFIG[:mongo_password])
+end
+
+if defined?(PhusionPassenger)
+   PhusionPassenger.on_event(:starting_worker_process) do |forked|
+     MongoMapper.connection.connect if forked
+   end
+end
+
+# The following indexes were created manually (in the background)
+# ** We are not running these programatically as they can be VERY slow! **
+# ** Do not use :index => true in your models **
+#
+# EXAMPLE  Used by index to get broadcasts for users channel, this needs to be very efficient as it affects loading of the app
+# EXAMPLE Broadcast.ensure_index([[:channel_id, 1], [:created_at, -1], [:video_player, 1]], :background => true)
+#
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CensoredTweets::Application.config.secret_token = '9312f2f37ef8c9534d64ea0d617200109752ed7a4b635cf1fc940754c79a25560c7dd0a336ae736e9740bf3a5a6d2d02de6224024c676f6b04aa4e15e4e85d5a'
