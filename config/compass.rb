09fedcd0d56d5bec8e579ccaf25bf183e541ee51
Henry <henry@sztul.com>
2011-06-25 18:08:46 -0400

    getting sass to work in heroku

--- config/compass.rb
+++ config/compass.rb
@@ -6,7 +6,7 @@ http_path = "/"
 environment = Compass::AppIntegration::Rails.env
 if environment == 'production'
   css_dir = "tmp/stylesheets"
-  sass_dir = "app/views/stylesheets"
+  sass_dir = "app/stylesheets"
 else
   css_dir = "public/stylesheets"
   sass_dir = "app/stylesheets"
